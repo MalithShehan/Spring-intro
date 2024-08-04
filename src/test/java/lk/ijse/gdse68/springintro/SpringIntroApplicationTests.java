32595b479a44d6bfbc4ffdde76bc425eb87874f3
MalithShehan <shehansandakalum2003@gmail.com>
2024-08-04 09:15:13 +0530

    Create Spring Intro

---
+++ src/test/java/lk/ijse/gdse68/springintro/SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse68.springintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
